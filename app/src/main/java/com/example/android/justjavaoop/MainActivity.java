6f9e612bd5b53ec49a1eca36c973f9517e7e457f
Roy Garcia <roygarcia201@gmail.com>
2018-04-10 14:27:55 -0700

    Followed the rest of the course and made final changes to the Project

--- app/src/main/java/com/example/android/justjavaoop/MainActivity.java
+++ app/src/main/java/com/example/android/justjavaoop/MainActivity.java
@@ -1,132 +1,147 @@
-/**
- * IMPORTANT: Make sure you are using the correct package name.
- * This example uses the package name:
- * package com.example.android.justjava
- * If you get an error when copying this code into Android studio, update it to match teh package name found
- * in the project's AndroidManifest.xml file.
- **/
-
 package com.example.android.justjavaoop;
 
-
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-import java.text.NumberFormat;
+import android.widget.Toast;
+
+import com.example.android.justjavaoop.R;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.example.android.justjavaoop.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
-
-    public void increment(View view){
-        quantity = quantity + 1;
-        displayQuantity(quantity );
-        displayPrice(calculatePrice(quantity));
-
-    }
-
-    public void decrement(View view){
-        if (quantity > 0) {
-            quantity = quantity - 1;
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+        if (quantity == 100) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
         }
+        quantity = quantity + 1;
         displayQuantity(quantity);
-        displayPrice(calculatePrice(quantity));
-
-
     }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view) {
+        if (quantity == 1) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
+        quantity = quantity - 1;
+        displayQuantity(quantity);
+    }
+
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view)
-    {
-        displayMessage(createOrderSummary(5));
+    public void submitOrder(View view) {
+        // Get the name
+        EditText nameLine = (EditText) findViewById(R.id.name_field);
+        String name = nameLine.getText().toString();
 
+        // Figure out if the user wants whipped cream topping
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        // Figure out if the user wants chocolate topping
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+        String subject = "Just Java Order for: " + name;
+
+        // Calculate the price
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+
+        // Display the order summary on the screen
+        String message = createOrderSummary(price, hasWhippedCream, hasChocolate, name);
+        composeEmail(subject , message);
+        displayMessage("Done!");
+    }
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @return total price
+     */
+    private int calculatePrice(Boolean hasWhippedCream , Boolean hasChocolate) {
+        int price = quantity * 5;
+        if (hasWhippedCream){
+            price += quantity;
+        }
+
+        if (hasChocolate){
+            price += quantity*2;
+        }
+        return price;
+    }
+
+    /**
+     * Create summary of the order.
+     * @param addName of the customer
+     * @param price           of the order
+     * @param addWhippedCream is whether or not to add whipped cream to the coffee
+     * @param addChocolate    is whether or not to add chocolate to the coffee
+     * @return text summary
+     */
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate, String addName) {
+        String priceMessage = "Name: ";
+
+        priceMessage += addName;
+        priceMessage += "\nAdd whipped cream? " + addWhippedCream;
+        priceMessage += "\nAdd chocolate? " + addChocolate;
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank you!";
+        return priceMessage;
+
+
+    }
+
+    public void composeEmail( String subject, String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void displayQuantity(int number) {
-        TextView quantityTextView = (TextView) findViewById(com.example.android.justjavaoop.R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-    }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(com.example.android.justjavaoop.R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private void displayQuantity(int numberOfCoffees) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + numberOfCoffees);
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(com.example.android.justjavaoop.R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
-
-    /**
-     * Calculates the price of the order based on the current quantity.
-     *
-     * @return the price
-     */
-    private int calculatePrice() {
-        int price = quantity * 5;
-        return price;
-    }
-
-
-    /**
-     * Calculates the price of the order.
-     *
-     * @param quantity is the number of cups of coffee ordered
-     */
-    private int calculatePrice(int quantity) {
-        int price = quantity * 5;
-        return price;
-    }
-
-    /**
-     * Calculates the price of the order.
-     *
-     * @param quantity is the number of cups of coffee ordered
-     * @param pricePerCoffee in the local pricePerCoffee
-     */
-    private int calculatePrice(int quantity, int pricePerCoffee) {
-        int price = quantity * pricePerCoffee;
-        return price;
-    }
-
-    /**
-     * Display the order summary.
-     *
-     */
-    private String createOrderSummary(int pricePerCoffee) {
-        String message = "Name: Roy Louis L. Garcia\n";
-        if (quantity <2){
-            message = message + "You ordered " + quantity + " coffee.";
-        }else{
-            message = message + "You ordered " + quantity + " coffees.";
-        }
-        message = message + "\n" + "Amount due: $" + calculatePrice(quantity, pricePerCoffee) + "\nThank you!";
-        return message;
-
-    }
-
-
-
 
 }
